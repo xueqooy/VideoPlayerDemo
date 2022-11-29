b4c3bbf880950e0ec4bc473725cc2e3360fa94e3
xuejianhui <106154926+xue-nd@users.noreply.github.com>
2022-11-29 19:10:12 +0800

    Initial Commit

---
+++ VideoPlayerPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VideoPlayerPractice
+//
+//  Created by xueqooy on 2022/11/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
